bf2ff8274cd2a29b434b99e59b9685308777a41c
Fred Morris <mfred7995@gmail.com>
2025-12-25 15:14:45 +0000

    File changes

--- src/components/map/CityMap.jsx
+++ src/components/map/CityMap.jsx
@@ -108,7 +108,7 @@ export default function CityMap({ activeUsers, currentUserProfile, userLocation,
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
         />
         <MapController center={mapCenter} />
         
