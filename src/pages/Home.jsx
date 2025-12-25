9121ac13b590b79f97bcaffa51c1562b513e3668
Fred Morris <mfred7995@gmail.com>
2025-12-25 04:32:04 +0000

    Location-based user visibility

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -45,9 +45,13 @@ export default function Home() {
   });
 
   const { data: activeUsers = [] } = useQuery({
-    queryKey: ['activeUsers'],
+    queryKey: ['activeUsers', userLocation?.city],
     queryFn: async () => {
       const profiles = await base44.entities.UserProfile.filter({ is_popped_up: true });
+      // Filter by same city if we have location
+      if (userLocation?.city) {
+        return profiles.filter((p) => p.current_city === userLocation.city);
+      }
       return profiles;
     },
     refetchInterval: 30000 // Refresh every 30 seconds
