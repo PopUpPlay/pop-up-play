b511dfd88d6a72ce2dbe4db703e013152b75a8c9
Fred Morris <mfred7995@gmail.com>
2025-12-25 04:35:15 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -7,14 +7,14 @@ import Chat from './pages/Chat';
 import Dashboard from './pages/Dashboard';
 import Discover from './pages/Discover';
 import EnterAccessCode from './pages/EnterAccessCode';
-import Home from './pages/Home';
 import Menu from './pages/Menu';
 import OnlineMembers from './pages/OnlineMembers';
 import Pricing from './pages/Pricing';
-import Profile from './pages/Profile';
 import SubscriptionSettings from './pages/SubscriptionSettings';
 import SubscriptionSuccess from './pages/SubscriptionSuccess';
 import VideoCall from './pages/VideoCall';
+import Profile from './pages/Profile';
+import Home from './pages/Home';
 import __Layout from './Layout.jsx';
 
 
@@ -28,14 +28,14 @@ export const PAGES = {
     "Dashboard": Dashboard,
     "Discover": Discover,
     "EnterAccessCode": EnterAccessCode,
-    "Home": Home,
     "Menu": Menu,
     "OnlineMembers": OnlineMembers,
     "Pricing": Pricing,
-    "Profile": Profile,
     "SubscriptionSettings": SubscriptionSettings,
     "SubscriptionSuccess": SubscriptionSuccess,
     "VideoCall": VideoCall,
+    "Profile": Profile,
+    "Home": Home,
 }
 
 export const pagesConfig = {
