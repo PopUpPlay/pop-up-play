457399ab213e7e5d129338b2954efd87c39dcebe
Fred Morris <mfred7995@gmail.com>
2025-12-30 17:14:24 +0000

    File changes

--- functions/sendBroadcast.ts
+++ functions/sendBroadcast.ts
@@ -38,6 +38,20 @@ Deno.serve(async (req) => {
 
     await base44.asServiceRole.entities.Message.bulkCreate(chatMessages);
 
+    // Send email notifications to all users
+    const emailPromises = allUsers
+      .filter(u => u.email !== user.email) // Don't email admin themselves
+      .map(u => 
+        base44.asServiceRole.integrations.Core.SendEmail({
+          to: u.email,
+          subject: subject || 'New Broadcast Message from Pop Up Play',
+          body: `You have received a new broadcast message.\n\n${subject ? `Subject: ${subject}\n\n` : ''}${message.trim()}\n\nLog in to Pop Up Play to view it in your messages.`
+        }).catch(err => console.error(`Failed to send email to ${u.email}:`, err))
+      );
+
+    // Send all emails in parallel
+    await Promise.all(emailPromises);
+
     return Response.json({
       success: true,
       broadcast_id: broadcast.id,
