cd98f77f0d70f84eb7ab8adbeb7a69e5e6ca6302
Fred Morris <mfred7995@gmail.com>
2025-12-30 02:24:15 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,4 +1,3 @@
-import About from './pages/About';
 import AccessCodeManager from './pages/AccessCodeManager';
 import AllProfiles from './pages/AllProfiles';
 import BlockedUsers from './pages/BlockedUsers';
@@ -6,19 +5,19 @@ import Broadcast from './pages/Broadcast';
 import Chat from './pages/Chat';
 import Dashboard from './pages/Dashboard';
 import EnterAccessCode from './pages/EnterAccessCode';
+import Home from './pages/Home';
 import Menu from './pages/Menu';
+import OnlineMembers from './pages/OnlineMembers';
 import Pricing from './pages/Pricing';
 import Profile from './pages/Profile';
 import SubscriptionSettings from './pages/SubscriptionSettings';
 import SubscriptionSuccess from './pages/SubscriptionSuccess';
 import VideoCall from './pages/VideoCall';
-import Home from './pages/Home';
-import OnlineMembers from './pages/OnlineMembers';
+import About from './pages/About';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
-    "About": About,
     "AccessCodeManager": AccessCodeManager,
     "AllProfiles": AllProfiles,
     "BlockedUsers": BlockedUsers,
@@ -26,14 +25,15 @@ export const PAGES = {
     "Chat": Chat,
     "Dashboard": Dashboard,
     "EnterAccessCode": EnterAccessCode,
+    "Home": Home,
     "Menu": Menu,
+    "OnlineMembers": OnlineMembers,
     "Pricing": Pricing,
     "Profile": Profile,
     "SubscriptionSettings": SubscriptionSettings,
     "SubscriptionSuccess": SubscriptionSuccess,
     "VideoCall": VideoCall,
-    "Home": Home,
-    "OnlineMembers": OnlineMembers,
+    "About": About,
 }
 
 export const pagesConfig = {
